a89684adc9720ac4a1456418256dec14e45d8882
Jan Kozłowski <240717@edu.p.lodz.pl>
2024-03-06 20:07:57 +0100

    Correcting Rozgardiasz Github ID

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print('grupa_pod_sciana\nGithub name scrum mastera: SelfishCrayfish\n\nJan Kozlowski: Developer, Github ID: Rozgardiasz\nBartosz Lysakowski: Developer\nKacper Zatonski: Developer\nMilosz Dzierzawski: DevOps\nArtur Trebski: Developer,Scrum Master ')
+print('grupa_pod_sciana\nGithub name scrum mastera: SelfishCrayfish\n\nJan Kozlowski: Developer, Github ID: 97686873\nBartosz Lysakowski: Developer\nKacper Zatonski: Developer\nMilosz Dzierzawski: DevOps\nArtur Trebski: Developer,Scrum Master ')
