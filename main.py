d8f56a34a3a7d003aa33ebd67102c7e5b3a25a49
Artur Trębski <trebski.artur@gmail.com>
2024-03-10 23:48:08 +0100

    Adding Github ID of Artur Trebski

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print('grupa_pod_sciana\nGithub name scrum mastera: SelfishCrayfish\n\nJan Kozlowski: Developer\nBartosz Lysakowski: Developer\nKacper Zatonski: Developer\nMilosz Dzierzawski: DevOps\nArtur Trebski: Developer,Scrum Master ')
+print('grupa_pod_sciana\nGithub name scrum mastera: SelfishCrayfish\n\nJan Kozlowski: Developer\nBartosz Lysakowski: Developer\nKacper Zatonski: Developer\nMilosz Dzierzawski: DevOps\nArtur Trebski: Developer, Scrum Master, Github ID: 137427463')
