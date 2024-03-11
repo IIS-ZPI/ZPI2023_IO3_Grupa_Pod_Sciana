4b8ef0af2607a7b5f00c8dd4706435c6653a6c7b
Kacper Zatoński <240844@edu.p.lodz.pl>
2024-03-12 00:38:11 +0100

    Fixed github id.

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print('grupa_pod_sciana\nGithub name scrum mastera: SelfishCrayfish\n\nJan Kozlowski: Developer\nBartosz Lysakowski: Developer\nKacper Zatonski: Developer, github id: 92331857\nMilosz Dzierzawski: DevOps\nArtur Trebski: Developer,Scrum Master')
+print('grupa_pod_sciana\nGithub name scrum mastera: SelfishCrayfish\n\nJan Kozlowski: Developer\nBartosz Lysakowski: Developer\nKacper Zatonski: Developer, Github ID: 92331857\nMilosz Dzierzawski: DevOps\nArtur Trebski: Developer,Scrum Master')
